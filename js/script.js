96f2023c0d2bf22723e04f205bf5745d8de0b0ef
Tatiane Machado <tatiane.nandam@gmail.comgit config --global user.email tatiane.nandam@gmail.com>
2024-05-04 01:25:04 -0300

    refatoração do codigo com arrorFunction e metodosjs

--- js/script.js
+++ js/script.js
@@ -1,61 +1,129 @@
 //lógica aqui
 
 //inicia uma lista vazia
-let estoque = []
+let estoque = [];
 
-let proximoId = 1; 
+let proximoId = 1;
+// //C => CRIAR
+const adicionarProdutoArrow = (nome, quantidade, preco, descricao) => {
+  let produto = {
+    id: proximoId++,
+    nome: nome,
+    quantidade: quantidade,
+    preco: preco,
+    descricao: descricao,
+  };
+  estoque.push(produto);
+  console.log(estoque);
+};
 
-//C => CRIAR 
-function adicionarProduto(nome, quantidade, preco, descricao) {
-        let produto = {
-            id: proximoId++,
-            nome: nome, 
-            quantidade: quantidade, 
-            preco: preco,
-            descricao: descricao
-        }
-        estoque.push(produto)
-}
+const listarProdutoArrow = () => {
+  let produto = 0;
+  estoque.forEach((elemento) => (produto = elemento));
+  console.log("continuando", produto);
+};
 
-// R(READ) => LER
-function listarProduto(){
-    for(let i=0; i < estoque.length; i++){
-        let produto = estoque[i];
-        console.log("continuando", produto)
+const atualizarProdutoArrow = (id, informacaoAtualizada) => {
+  const estoqueAtualizado = estoque.map((estoque) => {
+    if (estoque.id === id) {
+      return {
+        estoque: estoque,
+        informacaoAtualizada,
+        id: estoque.id,
+      };
     }
-}
+    return estoque, informacaoAtualizada, estoque.id;
+  });
+  console.log("Atulizada", estoqueAtualizado);
+};
 
+const removerProdutoArrow = (id) => {
+  if (id === estoque.id) estoque.forEach((remove) => remove.slice((remove, 1)));
+  console.log("Foi removido", estoque);
+};
 
-// U => Atualizar
-function atualizarProduto (id, informacaoAtualizada) {
-	for (let i = 0; i < estoque.length ; i++) {
-		if (estoque[i].id === id) {
-        	estoque[i] = Object.assign(estoque[i], informacaoAtualizada)
-          break;
-        }
-    }
-}
+adicionarProdutoArrow("Short", 1, 2000, "Shot Masculino");
+adicionarProdutoArrow("Calça", 4, 4000, "Calça jeans MAculino");
+adicionarProdutoArrow("Guarda-chuva", 10, 400, "Objeto");
+atualizarProdutoArrow(1, "Vestido");
+listarProdutoArrow();
+removerProdutoArrow(2);
+listarProdutoArrow();
 
-// D => Deletar
-function removerProduto(id){
-    for(let i = 0; i < estoque.length; i++){
-        if(estoque[i].id === id){
-            let removerProduto = estoque.splice(i, 1)
-            console.log("removeu: ", removerProduto)
-            return;
-        }
-    }
-}
+// //
+// function adicionarProduto(nome, quantidade, preco, descricao) {
+//   let produto = {
+//     id: proximoId++,
+//     nome: nome,
+//     quantidade: quantidade,
+//     preco: preco,
+//     descricao: descricao,
+//   };
+//   estoque.push(produto);
+// }
 
-let novoProduto1 = adicionarProduto("Tênis", "30", "12.00", "Tênis da marca Afrocódigos");
-let novoProduto2 = adicionarProduto("Tênis", "30", "12.00", "Tênis da marca Afrocódigos");
-let novoProduto3 = adicionarProduto("Tênis", "30", "12.00", "Tênis da marca Afrocódigos");
-let novoProduto4 = adicionarProduto("Tênis", "30", "12.00", "Tênis da marca Afrocódigos");
-let novoProduto5 = adicionarProduto("Tênis", "30", "12.00", "Tênis da marca Afrocódigos");
+// // // R(READ) => LER
+// function listarProduto() {
+//   for (let i = 0; i < estoque.length; i++) {
+//     let produto = estoque[i];
+//     console.log("continuando", produto);
+//   }
+// }
 
+// // // U => Atualizar
+// function atualizarProduto(id, informacaoAtualizada) {
+//   for (let i = 0; i < estoque.length; i++) {
+//     if (estoque[i].id === id) {
+//       estoque[i] = Object.assign(estoque[i], informacaoAtualizada);
+//       break;
+//     }
+//   }
+// }
 
-listarProduto()
+// // // D => Deletar
+// function removerProduto(id) {
+//   for (let i = 0; i < estoque.length; i++) {
+//     if (estoque[i].id === id) {
+//       let removerProduto = estoque.splice(i, 1);
+//       console.log("removeu: ", removerProduto);
+//       return;
+//     }
+//   }
+// }
 
-removerProduto(3)
+// let novoProduto1 = adicionarProduto(
+//   "Tênis",
+//   "30",
+//   "12.00",
+//   "Tênis da marca Afrocódigos"
+// );
+// let novoProduto2 = adicionarProduto(
+//   "Tênis",
+//   "30",
+//   "12.00",
+//   "Tênis da marca Afrocódigos"
+// );
+// let novoProduto3 = adicionarProduto(
+//   "Tênis",
+//   "30",
+//   "12.00",
+//   "Tênis da marca Afrocódigos"
+// );
+// let novoProduto4 = adicionarProduto(
+//   "Tênis",
+//   "30",
+//   "12.00",
+//   "Tênis da marca Afrocódigos"
+// );
+// let novoProduto5 = adicionarProduto(
+//   "Tênis",
+//   "30",
+//   "12.00",
+//   "Tênis da marca Afrocódigos"
+// );
 
-listarProduto()
+// listarProduto();
+
+// removerProduto(3);
+
+// listarProduto();
